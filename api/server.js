3668a17df0b383083dca70de5c41521bd68489ef
Ash Uppal <ashuppal@gmail.com>
2023-04-08 11:59:01 -0700

    added max file size limit

--- api/server.js
+++ api/server.js
@@ -12,8 +12,16 @@ const upload = multer({ storage: multer.memoryStorage() });
 const WHISPER_API_URL = 'https://api.openai.com/v1/audio/transcriptions';
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
+
 app.post('/whisper', upload.single('audio'), async (req, res) => {
   try {
+    const MAX_FILE_SIZE_MB = process.env.MAX_FILE_SIZE_MB || 25;
+    const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
+
+    if (req.file.size > MAX_FILE_SIZE_BYTES) {
+      return res.status(400).json({ error: `File size must be less than ${MAX_FILE_SIZE_MB}MB` });
+    }
+
     const audioFile = req.file.buffer;
     const formData = new FormData();
     formData.append('file', audioFile, req.file.originalname);
