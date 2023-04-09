716c5ba4fd20b877c0fed746095bf0aa1f48016f
Ash Uppal <ashuppal@gmail.com>
2023-04-08 17:37:48 -0700

    added styling

--- frontend/src/Components/Transcription.js
+++ frontend/src/Components/Transcription.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PptxGenJS from 'pptxgenjs';
 
+
 const Transcription = ({ audioFile }) => {
   const [transcription, setTranscription] = useState(null);
 
